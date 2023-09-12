6f60e2ed3a8b5f1613825ab2d9c69754cfac1521
Your Name <you@example.com>
2023-09-12 21:40:04 +0500

    Все функции бд прописаны кроме commentservice

---
+++ database/userservice.py
@@ -0,0 +1,88 @@
+from database.models import User
+
+from datetime import datetime
+from database import get_db
+
+
+# Регистрация пользователя
+def register_user_db(name, surname, email, phone_number, city, password):
+    db = next(get_db())
+
+    checker = db.query(User).filter_by(email=email).first()
+
+    if checker:
+        return False
+
+    new_user = User(name=name, surname=surname, email=email, phone_number=phone_number,
+                    city=city, password=password, reg_data=datetime.now())
+
+    db.add(new_user)
+    db.commit()
+
+    return 'Пользователь успешно зарегестрирован'
+
+
+# Вход в аккаунт
+def login_user_db(email, password):
+    db = next(get_db())
+
+    checker = db.query(User).filter_by(email=email).first()
+
+    if checker:
+        if checker.password == password:
+            return checker
+        elif checker.password != password:
+            return 'Неверный пароль'
+
+    return 'Ошибка в данных'
+
+
+# Добавить фото профиля
+def add_profile_photo_db(user_id, photo):
+    db = next(get_db())
+
+    checker = db.query(User).filter_by(id=user_id).first()
+
+    if checker:
+        checker.profile_photo = photo
+        db.commit()
+
+        return 'Фото профиля добавлено '
+
+    return False
+
+
+# Изменить данные
+def edit_user_db(user_id):
+    pass
+
+
+# Удалить фото профиля
+def delete_profile_photo_db(user_id):
+    db = next(get_db())
+
+    checker = db.query(User).filter_by(id=user_id).first()
+
+    if checker:
+        checker.profile_photo = user_id
+        db.commit()
+
+        return 'Фото профиля удалено'
+
+    return False
+
+# Получить всех пользователей
+def get_all_users_db():
+    db = next(get_db())
+
+    all_users = db.query(User).all()
+
+    return all_users
+
+# Получить информацию про определенного пользователя
+def get_exact_users_db(user_id):
+    db = next(get_db())
+
+    exact_user = db.query(User).filter_by(id=user_id).first()
+
+    return exact_user
--- user/user_api.py
+++ user/user_api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from user import RegisterUserModel, LoginUserModel, EditUserModel
 
 
 # Создаем компонент
@@ -7,23 +8,23 @@ user_router = APIRouter(prefix='/user', tags=['Управления пользо
 
 # запрос на вход в аккаунт
 @user_router.post('/login')
-async def login_user():
+async def login_user(data: LoginUserModel):
     pass
 
 
 # запрос на регистрацию
 @user_router.post('/register')
-async def register_user():
+async def register_user(data: RegisterUserModel):
     pass
 
 
 # запрос на изменения информации
 @user_router.put('/change_info')
-async def change_info_user():
+async def change_info_user(data: EditUserModel):
     pass
 
 
 # запрос на получения пользователя
 @user_router.get('/get_user')
-async def get_user():
+async def get_user(user_id: int):
     pass
